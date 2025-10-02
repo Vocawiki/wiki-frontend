3dc041d87321b87905a485b15ed19fdb03c3b713
AmeroHan <72120616+AmeroHan@users.noreply.github.com>
2025-10-02 19:02:05 +0800

    测试GitHub Actions部署到Vocawiki

--- src/gadgets/Cat-a-lot/(meta).ts
+++ src/gadgets/Cat-a-lot/(meta).ts
@@ -2,8 +2,8 @@ import type { GadgetMeta } from '@/tools/gadget/types'
 
 export default {
 	pages: [
-		{ type: 'existing', name: 'Cat-a-lot.js' },
 		{ type: 'existing', name: 'Cat-a-lot.css' },
+		{ type: 'existing', name: 'Cat-a-lot.js' },
 	],
 	withResourceLoader: true,
 	defaultEnabled: false,
