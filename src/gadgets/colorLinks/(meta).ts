00e59b156e7eb4285bcaea52d35fd8a41347fb71
亚衣琴 <Saoutax@gmail.com>
2025-12-07 12:46:44 +0800

    将标记跨维基和消歧义链接小工具纳入代码库

--- src/gadgets/colorLinks/(meta).ts
+++ src/gadgets/colorLinks/(meta).ts
@@ -1,7 +1,7 @@
 import type { GadgetMeta } from '@/tools/gadget/types'
 
 export default {
-	pages: [{ type: 'existing', name: 'colorLinks.css' }],
+	pages: [{ type: 'source', entry: 'colorLinks.scss' }],
 	withResourceLoader: true,
 	defaultEnabled: false,
 } satisfies GadgetMeta
