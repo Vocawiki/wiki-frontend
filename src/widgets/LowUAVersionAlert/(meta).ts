bc9c796c677b6dcf70101f5128e34dd9289dc141
AmeroHan <72120616+AmeroHan@users.noreply.github.com>
2025-12-20 01:18:05 +0800

    feat: 浏览器版本过低时展示警告

---
+++ src/widgets/LowUAVersionAlert/(meta).ts
@@ -0,0 +1,6 @@
+import type { WidgetMeta } from '@/tools/widget'
+
+export default {
+	description: '用于检测浏览器版本，在版本过低显示警告。',
+	script: { type: 'module' },
+} satisfies WidgetMeta
---
+++ src/widgets/LowUAVersionAlert/index.ts
@@ -0,0 +1,132 @@
+/**
+ * 这段代码需要单独拿出来，而不能放site-js里。
+ * 这是因为如果浏览器版本过低不支持site-js里的语法或函数，
+ * 会导致的检测低版本浏览器的代码永远无法被执行，失去了意义。
+ */
+
+const componentHTML = `<div
+	id="low-ua-version-alert"
+	class="preflight"
+	role="alertdialog"
+	aria-labelledby="low-ua-version-alert-label"
+	aria-describedby="low-ua-version-alert-desc"
+	style="
+		display: flex;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		bottom: env(safe-area-inset-bottom);
+		left: env(safe-area-inset-left);
+		right: env(safe-area-inset-right);
+		z-index: 200;
+		align-items: center;
+		justify-content: center;
+		pointer-events: none;
+	"
+>
+	<div
+		style="
+			padding: 1rem 0.875rem 1rem 1rem;
+			background-color: #cf2b65;
+			color: #ffffff;
+			pointer-events: auto;
+			border-radius: 0.5rem;
+			margin: 1rem 1rem 2rem;
+			/**
+			 * 这段代码需要单独拿出来，而不能放site-js里。
+			 * 这是因为如果浏览器版本过低不支持site-js里的语法或函数，
+			 * 会导致的检测低版本浏览器的代码永远无法被执行，失去了意义。
+			 */
+			box-shadow: 0 0.25rem 1rem rgba(207, 43, 101, 0.5);
+			line-height: 1.25;
+			display: flex;
+			align-items: center;
+		"
+	>
+		<div>
+			<span id="low-ua-version-alert-label"
+				>你的浏览器内核版本过低，网站内容与功能可能无法按预期呈现与生效，建议升级/更换浏览器。</span
+			><span id="low-ua-version-alert-desc"
+				>请查阅<a
+					href="/Help:%E6%B5%8F%E8%A7%88%E5%99%A8%E5%85%BC%E5%AE%B9%E6%80%A7"
+					title="Help:浏览器兼容性"
+					style="
+						color: #fff1b7;
+						text-decoration: underline;
+						text-underline-offset: 0.25em;
+						text-decoration-thickness: from-font;
+					"
+					>帮助:浏览器兼容性</a
+				>。</span
+			>
+		</div>
+		<button
+			type="button"
+			class="select-none"
+			aria-label="关闭提示"
+			onclick="document.getElementById('low-ua-version-alert').remove()"
+			style="
+				margin-left: 1rem;
+				background-color: #fff;
+				width: 1.25em;
+				height: 1.25em;
+				font-size: 1rem;
+				display: flex;
+				line-height: 1;
+				border-radius: 1em;
+				font-weight: 600;
+				align-items: center;
+				justify-content: center;
+				cursor: pointer;
+			"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				stroke="#cf2b65"
+				stroke-width="2.5"
+				stroke-linecap="round"
+				style="width: 1em; height: 1em"
+			>
+				<path d="M18 6 6 18"></path>
+				<path d="m6 6 12 12"></path>
+			</svg>
+		</button>
+	</div>
+</div>`
+
+/**
+ * 支持矩阵：
+ * - Windows 7最后一个版本：
+ *   - Chrome 109
+ *     https://developer.chrome.com/blog/new-in-chrome-109
+ *   - Firefox 115
+ *     https://www.firefox.com/en-US/firefox/115.0/releasenotes/
+ * - Safari 16.4
+ *   https://developer.apple.com/documentation/safari-release-notes/safari-16_4-release-notes
+ */
+function doesUAMeetMinimumVersion(): boolean {
+	if (CSS.supports('width', '1lh')) {
+		// Chrome >= 109, Safari >= 16.4, Firefox >= 120
+		return true
+	}
+
+	if (!CSS.supports('animation-composition', 'add')) {
+		// Chrome < 112, Safari < 16.0, Firefox < 115
+		return false
+	}
+	// 16.0 <= Safari < 16.4, 115 <= Firefox < 120
+
+	if (CSS.supports('selector(:dir(ltr))')) {
+		// Firefox >= 115
+		return true
+	}
+
+	// 16.0 <= Safari < 16.4
+	return false
+}
+
+if (!doesUAMeetMinimumVersion()) {
+	document.body.insertAdjacentHTML('beforeend', componentHTML)
+}
