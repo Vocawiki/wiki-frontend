f9b3198d49187404320a92c8d3907b850b42f1df
Wecury <110245145+Wecury@users.noreply.github.com>
2026-02-01 08:05:30 +0800

    fix: WikiEditor在citizen皮肤的定位错误 (#4)

--- src/gadgets/InPageEdit-NEXT/(meta).ts
+++ src/gadgets/InPageEdit-NEXT/(meta).ts
@@ -1,7 +1,10 @@
 import type { GadgetMeta } from '@/tools/gadget/types'
 
 export default {
-	pages: [{ type: 'source', entry: 'InPageEdit-NEXT.js' }],
+	pages: [
+		{ type: 'existing', name: 'InPageEdit-NEXT.css' },
+		{ type: 'source', entry: 'InPageEdit-NEXT.js' },
+	],
 	withResourceLoader: true,
 	defaultEnabled: false,
 	type: 'general',
