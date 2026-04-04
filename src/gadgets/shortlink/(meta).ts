b3850f4f2ae665ff311a9875aa927fdd61bf8a9b
亚衣琴 <Saoutax@gmail.com>
2026-04-04 17:55:46 +0800

    fix: 依赖缺失

--- src/gadgets/shortlink/(meta).ts
+++ src/gadgets/shortlink/(meta).ts
@@ -4,4 +4,5 @@ export default {
 	pages: [{ type: 'existing', name: 'shortlink.js' }],
 	withResourceLoader: true,
 	defaultEnabled: false,
+	dependencies: ['ext.gadget.site-lib', 'mediawiki.widgets'],
 } satisfies GadgetMeta
