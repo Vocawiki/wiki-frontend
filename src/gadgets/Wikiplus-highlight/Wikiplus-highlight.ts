264bd656e0f7ebc9fc8c9904ee253eb1d2d034b4
AdorN <40393619+Ador-N@users.noreply.github.com>
2026-06-20 17:08:39 +0800

    fix: 临时修复 Wikiplus-highlight

--- src/gadgets/Wikiplus-highlight/Wikiplus-highlight.ts
+++ src/gadgets/Wikiplus-highlight/Wikiplus-highlight.ts
@@ -1,6 +1,8 @@
 void (async () => {
 	const { wgIsArticle, wgAction } = mw.config.get(['wgIsArticle', 'wgAction'])
 	if (wgIsArticle && wgAction === 'view') {
+		// @ts-expect-error - mw.libs is used for assigning third-party libraries
+		mw.libs.wphl = { cmVersion: 6 }
 		await libCachedCode.injectCachedCode(
 			'https://testingcf.jsdelivr.net/npm/wikiplus-highlight@latest',
 			'script',
