f18e97172c2d66d754ec7d3c5ebef5cdbe3a5a14
AmeroHan <72120616+AmeroHan@users.noreply.github.com>
2026-07-24 10:02:54 +0800

    chore: 更改CSS工具类force-inline-full至ignore-article-inline-padding，新增ignore-article-max-width

--- src/templates/首页/bilibili-videos.tsx
+++ src/templates/首页/bilibili-videos.tsx
@@ -68,7 +68,7 @@ export function BilibiliVideos() {
 					<Wiki.Link href={accountURL}>关注我们的官方账号</Wiki.Link>
 				</div>
 			</div>
-			<ul className="grid grid-cols-2 gap-1.5 not-main-2xs:force-inline-full not-main-2xs:pr-[max(6px,env(safe-area-inset-right))] not-main-2xs:pl-[max(6px,env(safe-area-inset-left))] main-2xs:gap-2 main-sm:grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] main-sm:gap-3">
+			<ul className="grid grid-cols-2 gap-1.5 not-main-2xs:ignore-article-inline-padding not-main-2xs:pr-[max(6px,env(safe-area-inset-right))] not-main-2xs:pl-[max(6px,env(safe-area-inset-left))] main-2xs:gap-2 main-sm:grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] main-sm:gap-3">
 				{videos
 					.toSorted((a, b) => b.uploadedAt.getTime() - a.uploadedAt.getTime())
 					.map((video) => (
