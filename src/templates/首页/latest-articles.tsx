6807ba0e321d94395a3102fc15d6c03a4fcba668
AmeroHan <72120616+AmeroHan@users.noreply.github.com>
2026-07-24 16:37:39 +0800

    feat: 增加渐隐 更好的滚动条、拖拽滚动等睡醒了再说

--- src/templates/首页/latest-articles.tsx
+++ src/templates/首页/latest-articles.tsx
@@ -3,7 +3,16 @@ export function LatestArticles() {
 		<div>
 			<h2 className="text-center">最新条目</h2>
 			{'{{#widget:LatestArticleList}}'}
-			<div id="latest-article-list" className="preflight ignore-article-max-width">
+			<div
+				id="latest-article-list"
+				className="preflight ignore-article-max-width"
+				style={{
+					WebkitMaskImage:
+						'-webkit-linear-gradient(left, transparent, black calc(50cqw - var(--width-layout) / 2), black calc(50cqw + var(--width-layout) / 2), transparent)',
+					maskImage:
+						'linear-gradient(to right, transparent, black calc(50cqw - var(--width-layout) / 2), black calc(50cqw + var(--width-layout) / 2), transparent)',
+				}}
+			>
 				{/* @ts-expect-error MW标签 */}
 				<dynamicpagelist>
 					{`
