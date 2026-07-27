6c62f935bfaf02215f4870a2f3ae6654b964624e
AmeroHan <72120616+AmeroHan@users.noreply.github.com>
2026-07-27 15:20:06 +0800

    chore: 优化滚动条动画

--- src/widgets/LatestArticleList/page-list.tsx
+++ src/widgets/LatestArticleList/page-list.tsx
@@ -91,12 +91,12 @@ export function HorizontalScrollablePageList({ pages }: { pages: PartialPageInfo
 				</ScrollArea.Content>
 			</ScrollArea.Viewport>
 			<ScrollArea.Scrollbar
-				className="pointer-events-none relative flex h-5 max-w-(--width-layout) items-center rounded-max bg-transparent transition-colors active:*:ease-out data-hovering:pointer-events-auto data-hovering:bg-(--background-color-neutral-subtle) data-scrolling:pointer-events-auto data-scrolling:bg-(--background-color-neutral-subtle) data-scrolling:duration-0"
+				className="pointer-events-none relative flex h-5 max-w-(--width-layout) items-center rounded-max bg-transparent transition-colors data-hovering:pointer-events-auto data-hovering:bg-(--background-color-neutral-subtle) data-scrolling:pointer-events-auto data-scrolling:bg-(--background-color-neutral-subtle)"
 				orientation="horizontal"
 			>
-				<ScrollArea.Thumb className="group/thumb pointer-events-auto relative h-full w-full">
-					<div className="absolute inset-1.5 rounded-max bg-(--background-color-progressive)/15 transition-[inset] group-active/thumb:-inset-1" />
-					<div className="absolute inset-1.75 rounded-max bg-(--background-color-progressive) opacity-80 transition-[opacity,inset,box-shadow] group-hover/thumb:inset-1 group-hover/thumb:opacity-100 group-active/thumb:inset-1.5 group-active/thumb:opacity-100 group-active/thumb:duration-75" />
+				<ScrollArea.Thumb className="group/thumb pointer-events-auto relative h-full w-full [--inset:7px] hover:[--inset:4px] active:[--inset:6px] active:*:ease-out">
+					<div className="absolute inset-(--inset) rounded-max bg-(--background-color-progressive)/15 transition-[inset] group-active/thumb:-inset-1 group-active/thumb:duration-100" />
+					<div className="absolute inset-(--inset) rounded-max bg-(--background-color-progressive) opacity-80 transition-[opacity,inset,box-shadow] group-hover/thumb:opacity-100 group-active/thumb:opacity-100 group-active/thumb:duration-75" />
 				</ScrollArea.Thumb>
 			</ScrollArea.Scrollbar>
 		</ScrollArea.Root>
